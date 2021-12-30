68933a37e5146ca5203c3c2ed22fa9852b7bacbd
Stefan Annell <steann@kth.se>
2021-12-30 14:37:43 +0100

    5

--- main.cpp
+++ main.cpp
@@ -2,7 +2,21 @@
 #include <iostream>
 #include "ray.h"
 
+bool hit_sphere(const point3& center, float radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  float a = dot(r.direction(), r.direction());
+  float b = 2.0 * dot(oc, r.direction());
+  float c = dot(oc, oc) - radius * radius;
+  float discriminant = b * b - 4 * a * c;
+
+  return discriminant > 0;
+}
+
 color ray_color(const ray& r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    return color(1, 0, 0);
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
   float t = 0.5 * (unit_direction.y() + 1.0);
 
