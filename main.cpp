56a79b0c4f8fb443b8a10f61d4bf2665ee70c797
Stefan Annell <steann@kth.se>
2021-12-30 13:28:03 +0100

    2.3

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main() {
   std::cout << "P3" << std::endl << image_width << ' ' << image_height << std::endl << "255" << std::endl;
 
   for (int j = image_height - 1; j >= 0; j--) {
+    std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
     for (int i = 0; i < image_width; i++) {
       float r = float(i) / (image_width - 1);
       float g = float(j) / (image_height - 1);
@@ -21,6 +22,7 @@ int main() {
       std::cout << ir << ' ' << ig << ' ' << ib << std::endl;
     }
   }
+  std::cerr << std::endl << "Done!" << std::endl;
 
   return 0;
 }
